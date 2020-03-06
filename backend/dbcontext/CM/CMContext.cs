efb08f82042fe429be41368bc7b0723837b79a62
r.uchiyama <r.uchiyama@sint.co.jp>
2020-03-06 21:46:07 +0900

    add CM dbcontext

---
+++ backend/dbcontext/CM/CMContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CM.Models;
+
+namespace CM
+{
+    public class CMContext : DbContext
+    {
+      public CMContext(DbContextOptions<CMContext> options) : base(options) { }
+
+      public DbSet<Emp> Emp { get; set; }
+    }
+}
---
+++ backend/dbcontext/CM/Models/Emp.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CM.Models
+{
+    public class Emp
+    {
+      public int id {get;set;}
+      public string FirstName {get;set;}
+      public string LastName {get;set;}
+    }
+}
