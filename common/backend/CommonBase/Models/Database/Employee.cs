cfa6eb029483326b154b7efe60c3d99dc6e4ed6a
r.uchiyama <r.uchiyama@sint.co.jp>
2020-03-09 20:00:17 +0900

    update add column and key

--- common/backend/CommonBase/Models/Database/Employee.cs
+++ common/backend/CommonBase/Models/Database/Employee.cs
@@ -1,12 +1,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Common.CommonBase.Models.Database
 {
     public class Employee
     {
-      public int id {get;set;}
-      public string FirstName {get;set;}
-      public string LastName {get;set;}
+      [Key]
+      public Guid EmployeeId {get; set; }
+
+      public int EmployeeCode {get; set; }
+      public string FirstName {get; set; }
+      public string MiddleName {get; set; }
+      public string LastName {get; set; }
+      public string NickName {get; set; }
+      public bool ValidFlg{get; set; }
     }
 }
