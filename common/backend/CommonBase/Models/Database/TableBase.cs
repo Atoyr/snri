1b01f98586f44790513f758cceb335b20d009b1e
r.uchiyama <r.uchiyama@sint.co.jp>
2020-03-09 20:05:47 +0900

    update Column Date -> At

--- common/backend/CommonBase/Models/Database/TableBase.cs
+++ common/backend/CommonBase/Models/Database/TableBase.cs
@@ -6,12 +6,12 @@ namespace Common.CommonBase.Models.Database
     public class TableBase
     {
       public string CreatedBy {get; set; }
-      public DateTime CreateDate {get; set; }
+      public DateTime CreateAt {get; set; }
       public string ModifyBy {get; set; }
-      public DateTime ModifyDate {get; set; }
+      public DateTime ModifyAt {get; set; }
       public string SystemCreatedBy {get; set; }
-      public DateTime SystemCreateDate {get; set; }
+      public DateTime SystemCreateAt {get; set; }
       public string SystemModifyBy {get; set; }
-      public DateTime SystemModifyDate {get; set; }
+      public DateTime SystemModifyAt {get; set; }
     }
 }
