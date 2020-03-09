166ca37858fe49b42f014ceab65201f747544560
r.uchiyama <r.uchiyama@sint.co.jp>
2020-03-09 20:02:25 +0900

    add System Create and Modify Column at TableBase

--- common/backend/CommonBase/Models/Database/TableBase.cs
+++ common/backend/CommonBase/Models/Database/TableBase.cs
@@ -9,5 +9,9 @@ namespace Common.CommonBase.Models.Database
       public DateTime CreateDate {get; set; }
       public string ModifyBy {get; set; }
       public DateTime ModifyDate {get; set; }
+      public string SystemCreatedBy {get; set; }
+      public DateTime SystemCreateDate {get; set; }
+      public string SystemModifyBy {get; set; }
+      public DateTime SystemModifyDate {get; set; }
     }
 }
